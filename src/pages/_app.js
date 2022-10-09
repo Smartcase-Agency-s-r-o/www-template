fab73802f0ef8de3292396b457c79b35e1ad53b4
Lukas Alois Zbornik <l.a.zbornik@gmail.com>
2022-10-09 13:34:32 +0200

    Add Google Analytics Add Facebook Pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,68 @@
 import '../../styles/globals.css';
 
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+import * as gtag from '../../lib/gtag';
+import * as fbq from '../../lib/fpixel';
+
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    fbq.pageView();
+
+    const handleRouteChange = (url) => {
+      gtag.pageView(url);
+      fbq.pageView();
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
